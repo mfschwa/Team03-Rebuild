a964d54116cedf6a0b60ea2e321ea9fea67c800a
jrusso2 <jrusso2@g.clemson.edu>
2024-11-11 19:28:02 -0500

    warning fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useAuthenticator } from '@aws-amplify/ui-react';
 import { getCurrentUser, fetchAuthSession } from 'aws-amplify/auth';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import NavBar from "./ui/NavBar";
 import Profile from "./pages/Profile";
